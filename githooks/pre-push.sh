d5d65c38c1a824fecec2abe2ddcd67b1713a051f
Haru on Acer PC <s1230038uaizuacjp@gmail.com>
2024-03-28 17:41:57 +0900

    [add] pre-push hook

---
+++ githooks/pre-push.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+echo 
+echo '--- Execute Git hook ---------------'
+echo 'For more information, see .git/hooks/pre-push'
+echo
+echo '*** Bash script ***'
+echo '* Lint runs'
+./lint.sh
+
+echo '*** Bash completed ***'
+
+echo
+echo '--- Completed execuiting Git hook ---------------'
+echo
