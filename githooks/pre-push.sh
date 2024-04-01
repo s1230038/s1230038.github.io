a170578edc83aff273fa33381613f92d1c8d13db
Haru on Acer PC <s1230038uaizuacjp@gmail.com>
2024-04-01 18:20:00 +0900

    [delete] lint.sh in pre-push hook

--- githooks/pre-push.sh
+++ githooks/pre-push.sh
@@ -4,12 +4,6 @@ echo
 echo '--- Execute Git hook ---------------'
 echo 'For more information, see .git/hooks/pre-push'
 echo
-echo '*** Bash script ***'
-echo '* Lint runs'
-./lint.sh
-
-echo '*** Bash completed ***'
-
 echo
 echo '--- Completed execuiting Git hook ---------------'
 echo
